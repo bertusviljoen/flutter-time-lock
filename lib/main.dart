2074ae9502823697ebbc108a4a8bbe147298640a
Bertus Viljoen <viljoen.bertus@yahoo.com>
2024-12-16 09:05:13 +0200

    Add configuration state to check and display configuration file
    Fixes #6
    
    Add configuration file handling and display in `lib/main.dart`.
    
    * Import `dart:io` and `dart:convert` libraries.
    * Define the path to the configuration file.
    * Add code to check if the configuration file exists and create it if it doesn't.
    * Add code to read the configuration file and parse the JSON content.
    * Add code to display the configuration values on the screen using Flutter widgets.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/bertusviljoen/flutter_time_lock/issues/6?shareId=XXXX-XXXX-XXXX-XXXX).

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -12,12 +14,69 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Time Lock'),
-        ),
-        body: Center(
-          child: Text('Welcome to Flutter Time Lock!'),
+      home: ConfigurationScreen(),
+    );
+  }
+}
+
+class ConfigurationScreen extends StatefulWidget {
+  @override
+  _ConfigurationScreenState createState() => _ConfigurationScreenState();
+}
+
+class _ConfigurationScreenState extends State<ConfigurationScreen> {
+  Map<String, dynamic> config = {
+    'childPin': '',
+    'adultPin': '',
+    'lockTime': '',
+    'lockInterval': ''
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAndCreateConfigFile();
+  }
+
+  Future<void> _checkAndCreateConfigFile() async {
+    final directory = await Directory.systemTemp.createTemp();
+    final path = '${directory.path}/config.json';
+    final file = File(path);
+
+    if (await file.exists()) {
+      final contents = await file.readAsString();
+      setState(() {
+        config = jsonDecode(contents);
+      });
+    } else {
+      final defaultConfig = {
+        'childPin': '1234',
+        'adultPin': '5678',
+        'lockTime': '10',
+        'lockInterval': '20'
+      };
+      await file.writeAsString(jsonEncode(defaultConfig));
+      setState(() {
+        config = defaultConfig;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Time Lock'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Child Pin: ${config['childPin']}'),
+            Text('Adult Pin: ${config['adultPin']}'),
+            Text('Lock Time: ${config['lockTime']} mins'),
+            Text('Lock Interval: ${config['lockInterval']} mins'),
+          ],
         ),
       ),
     );
